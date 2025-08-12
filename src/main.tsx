fee8fc67b5ae2a2093db9e09a37f63672e87d68b
Alexandru <mjukilo@tutamail.com>
2025-08-12 21:49:38 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { WalletKit } from "./wallet";
+import "@solana/wallet-adapter-react-ui/styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <WalletKit>
+      <App />
+    </WalletKit>
   </React.StrictMode>
 );
