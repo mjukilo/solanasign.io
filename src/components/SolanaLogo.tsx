a6215c9fbebc9232585bff10befdc0844a40b926
Alexandru <mjukilo@tutamail.com>
2025-08-13 22:18:08 +0200

    Delete src directory

--- src/components/SolanaLogo.tsx
+++
@@ -1,9 +0,0 @@
-import solanaLogo from '../assets/solana-logo.svg'
-
-interface SolanaLogoProps {
-  className?: string;
-}
-
-export const SolanaLogo = ({ className = "w-16 h-16" }: SolanaLogoProps) => (
-  <img src={solanaLogo} alt="Solana Logo" className={className} />
-);
