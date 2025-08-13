0cc3808260f3a219e764176415532c2348447501
Alexandru <mjukilo@tutamail.com>
2025-08-13 20:01:17 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,32 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import { clusterApiUrl } from "@solana/web3.js";
-import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
-import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
-import { SolflareWalletAdapter } from "@solana/wallet-adapter-solflare";
-// Ajoute ici d’autres adapters si besoin (Backpack, Glow, Exodus...)
-import "@solana/wallet-adapter-react-ui/styles.css";
-
-const endpoint = clusterApiUrl("mainnet-beta");
-
-const wallets = [
-  new PhantomWalletAdapter(),
-  new SolflareWalletAdapter(),
-  // new BackpackWalletAdapter(),
-  // new GlowWalletAdapter(),
-  // new ExodusWalletAdapter(),
-];
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={wallets} autoConnect>
-        <WalletModalProvider>
-          <App />
-        </WalletModalProvider>
-      </WalletProvider>
-    </ConnectionProvider>
+    <App />
   </React.StrictMode>
 );
